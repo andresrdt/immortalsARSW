5a7555a560098c0ab88ab98e8c530c9e151d7600
andresrdt <andres.rodriguez-de@mail.escuelaing.edu.co>
2019-08-27 16:59:29 -0500

    invariante sirve y immortales muertos no reviven

--- src/main/java/edu/eci/arsw/highlandersim/Immortal.java
+++ src/main/java/edu/eci/arsw/highlandersim/Immortal.java
@@ -5,10 +5,10 @@ import java.util.Random;
 
 public class Immortal extends Thread {
 
-    private ImmortalUpdateReportCallback updateCallback=null;
-    
+    private ImmortalUpdateReportCallback updateCallback = null;
+
     private int health;
-    
+
     private int defaultDamageValue;
 
     private final List<Immortal> immortalsPopulation;
@@ -16,35 +16,33 @@ public class Immortal extends Thread {
     private final String name;
 
     private final Random r = new Random(System.currentTimeMillis());
-    
-    private boolean pause;
 
+    private boolean pause;
 
     public Immortal(String name, List<Immortal> immortalsPopulation, int health, int defaultDamageValue, ImmortalUpdateReportCallback ucb) {
         super(name);
-        this.updateCallback=ucb;
+        this.updateCallback = ucb;
         this.name = name;
         this.immortalsPopulation = immortalsPopulation;
         this.health = health;
-        this.defaultDamageValue=defaultDamageValue;
+        this.defaultDamageValue = defaultDamageValue;
     }
 
     public void run() {
-        pause=false;
+        pause = false;
         while (true) {
-        	if (pause) {
-        		try {
-        			synchronized(this) {
-        				this.wait();
-        			}
-					
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-        	}
+            if (pause) {
+                try {
+                    synchronized (this) {
+                        this.wait();
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             Immortal im;
 
-            
             int myIndex = immortalsPopulation.indexOf(this);
 
             int nextFighterIndex = r.nextInt(immortalsPopulation.size());
@@ -64,33 +62,39 @@ public class Immortal extends Thread {
                 e.printStackTrace();
             }
 
-        
-
-    }}
+        }
+    }
 
     public void fight(Immortal i2) {
+        synchronized (immortalsPopulation) {
+            if (this.getHealth() == 0) {
 
-        if (i2.getHealth() > 0) {
-            i2.changeHealth(i2.getHealth() - defaultDamageValue);
-            this.health += defaultDamageValue;
-            updateCallback.processReport("Fight: " + this + " vs " + i2+"\n");
-        } else {
-            updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
+            } else {
+                if (i2.getHealth() > 0) {
+                    i2.changeHealth(i2.getHealth() - defaultDamageValue);
+                    this.health += defaultDamageValue;
+                    updateCallback.processReport("Fight: " + this + " vs " + i2 + "\n");
+                } else {
+                    updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
+                }
+            }
         }
 
     }
-    public void pause(){
-        pause=true;
-        
+
+    public void pause() {
+        pause = true;
+
     }
-    
+
     public void Resume() {
-    	pause=false;
-    	synchronized(this) {
-    		this.notify();
-    	}
-    	
+        pause = false;
+        synchronized (this) {
+            this.notify();
+        }
+
     }
+
     public void changeHealth(int v) {
         health = v;
     }
